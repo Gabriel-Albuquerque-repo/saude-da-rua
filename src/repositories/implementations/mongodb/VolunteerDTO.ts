ac24b88e43ebf507988367a113080bab10eba237
Gabriel de Albuquerque <gabrieleac2009@gmail.com>
2021-12-01 12:10:33 -0300

    initial commit

---
+++ src/repositories/implementations/mongodb/VolunteerDTO.ts
@@ -0,0 +1,35 @@
+import { FreeDaysOfWeek, OccupationOptions } from '@entities/Volunteer';
+
+interface IVolunteer {
+    _id: string,
+
+    fullName: string,
+
+    email: string,
+
+    cellphoneNumberWithDDD: string,
+
+    occupation: OccupationOptions | string,
+
+    expertise: string,
+
+    listfreeDaysOfWeek: Array<FreeDaysOfWeek>,
+
+    numberOfFreeDaysOfWeek: number,
+
+    experienceWithHealthy: string,
+
+    didParticipate: boolean,
+
+    numberOfParticipation: number,
+
+    howDidknowOfSDR: string,
+}
+
+// Pega as opções dos enums que vêm da entidade Volunteer
+// Precisa disso para poder instanciar dentro do tipo enum no Schema
+const listEnumOccupation: Array<string> = Object.values(OccupationOptions);
+
+const listEnumFreeDays: Array<string> = Object.values(FreeDaysOfWeek);
+
+export { IVolunteer, listEnumOccupation, listEnumFreeDays };
--- src/repositories/implementations/mongodb/schemas/Volunteer.ts
+++ src/repositories/implementations/mongodb/schemas/Volunteer.ts
@@ -1,5 +1,88 @@
-import { Schema, model, Document } from 'mongoose';
+import { Schema, model } from 'mongoose';
+import { IVolunteer, listEnumFreeDays, listEnumOccupation } from '../VolunteerDTO';
 
-interface IVolunteer extends Document {
+// OBS.: Saber máximos e mínimos do tamanho de strings livres
 
-}
+const schema = new Schema<IVolunteer>({
+  _id: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+
+  cellphoneNumberWithDDD: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+
+  occupation: {
+    type: String,
+    required: true,
+    enum: listEnumOccupation,
+  },
+
+  expertise: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+
+  listfreeDaysOfWeek: [
+    {
+      type: String,
+      required: true,
+      enum: listEnumFreeDays,
+    },
+  ],
+
+  numberOfFreeDaysOfWeek: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 7,
+  },
+
+  experienceWithHealthy: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+
+  didParticipate: {
+    type: Boolean,
+    required: true,
+  },
+
+  numberOfParticipation: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 99,
+  },
+
+  howDidknowOfSDR: {
+    type: String,
+    required: true,
+  },
+}, {
+  timestamps: true,
+  strict: false,
+});
+
+// 'volunteers' é o nome da coleção
+export default model<IVolunteer>('Volunteer', schema, 'volunteers');
