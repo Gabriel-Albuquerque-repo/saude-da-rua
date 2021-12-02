4d4f03b83af412712d3613bf857d22c077f107d4
Gabriel de Albuquerque <gabrieleac2009@gmail.com>
2021-12-02 17:03:33 -0300

    [UPD] type of Promise

--- src/repositories/IVolunteerRepository.ts
+++ src/repositories/IVolunteerRepository.ts
@@ -1,9 +1,10 @@
 /* eslint-disable no-unused-vars */
 import { Volunteer } from '@entities/Volunteer';
+import { Document } from 'mongoose';
 
 interface IVolunteerRepository {
     findByEmail(email: string): Promise<Volunteer>;
-    saveVolunteer(volunteer: Volunteer): Promise<void>;
+    saveVolunteer(volunteer: Volunteer): Promise<Document<void>>;
 }
 
 export default IVolunteerRepository;
