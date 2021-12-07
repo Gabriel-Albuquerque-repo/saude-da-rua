f3cb5567098e35c2f1ad0d6b05b38aa3a4679ec8
Gabriel de Albuquerque <gabrieleac2009@gmail.com>
2021-12-07 19:42:46 -0300

    [FIX] add params to inject in create Controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import createVolunteerController from '@useCases/createVolunteer';
+import { createVolunteerController } from '@useCases/createVolunteer';
 
 // implementar utils/validações
 const router = Router();
 
-router.post('/create', createVolunteerController.handle);
+router.post('/create', (request, response) => {
+  createVolunteerController.handle(request, response);
+});
 
 export default router;
