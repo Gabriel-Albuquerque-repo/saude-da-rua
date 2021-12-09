fcd2f05bb19133508a716c6e3ee2fdce6ffc83bf
Gabriel de Albuquerque <gabrieleac2009@gmail.com>
2021-12-09 16:06:06 -0300

    [UPD] add form route and change design

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import { createVolunteerController } from '@useCases/createVolunteer';
+import { createFormController } from '@useCases/createForm';
 
 // implementar utils/validações
 const router = Router();
 
-router.post('/create', (request, response) => {
+router.post('/create-volunteer', (request, response) => {
   createVolunteerController.handle(request, response);
+});
+
+router.post('/submit-form', (request, response) => {
+  createFormController.handle(request, response);
 });
 
 export default router;
