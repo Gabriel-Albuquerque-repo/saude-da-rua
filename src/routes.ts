e2dc5a4edff49f47e63d5ba46072bbdfb76526a8
Gabriel de Albuquerque <gabrieleac2009@gmail.com>
2021-12-15 17:16:18 -0300

    [UPD] add initial createUserValidation

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,21 @@
 import { Router } from 'express';
 import { createVolunteerController } from '@useCases/createVolunteer';
 import { createFormController } from '@useCases/createForm';
+import CreateUserValidation from '@utils/validations/CreateUserValidation';
 
-// implementar utils/validações
 const router = Router();
 
-router.post('/create-volunteer', (request, response) => {
-  createVolunteerController.handle(request, response);
-});
+router.post(
+  '/create-volunteer',
+  (request, response, next) => {
+    // Má prática? Dee-se armazenar numa variável e fazer a chamada da função?
+    // Poderia colocar dentro do construtor o conteúdo da função?
+    new CreateUserValidation(request, response, next).validateInput();
+  },
+  (request, response) => {
+    createVolunteerController.handle(request, response);
+  },
+);
 
 router.post('/submit-form', (request, response) => {
   createFormController.handle(request, response);
