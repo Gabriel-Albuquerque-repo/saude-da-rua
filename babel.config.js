12c6898a1a28b68bd038629f3c9425134f8b874e
Gabriel de Albuquerque <gabrieleac2009@gmail.com>
2021-12-20 23:24:33 -0300

    [UPD] schemas alias

--- babel.config.js
+++ babel.config.js
@@ -20,7 +20,7 @@ module.exports = {
         '@repositories': 'src/repositories',
         '@mongoConnection': 'src/repositories/implementations/MongoConnection.ts',
         '@routes': 'src/routes.ts',
-        '@schemas': 'src/repositories/implementations/mongodb/schemas/Volunteer.ts'
+        '@schemas': 'src/repositories/implementations/mongodb/schemas'
       },
     }],
   ],
