d627c2f94a7c1f9e54741a3319c24436d203a7fe
Gabriel de Albuquerque <gabrieleac2009@gmail.com>
2021-12-23 15:29:47 -0300

    [ADD] test of existing volunteer

--- tests/unit/CreateVolunteerUseCase.spec.ts
+++ tests/unit/CreateVolunteerUseCase.spec.ts
@@ -1,16 +1,22 @@
-import MongoVolunteerRepository from '../../src/repositories/implementations/mongodb/repository/MongoVolunteerRepository';
+import VolunteerRepositoryInMemory from '../../in-memory/VolunteerRepositoryInMemory';
 import { OccupationOptions, FreeDaysOfWeek } from '../../src/entities/Volunteer';
 import ICreateVolunteerRequestDTO from '../../src/useCases/createVolunteer/CreateVolunteerDTO';
 import CreateVolunteerUseCase from '../../src/useCases/createVolunteer/CreateVolunteerUseCase';
+import IVolunteerRepository from '../../src/repositories/interfaces/IVolunteerRepository';
 
 describe('Create volunteer', () => {
-  it('should be able to create a new volunteer', async () => {
-    const volunteersRepository = new MongoVolunteerRepository();
-    const createVolunteerUseCase = new CreateVolunteerUseCase(volunteersRepository);
+  let volunteersRepositoryInMemory: IVolunteerRepository;
+  let createVolunteerUseCase: CreateVolunteerUseCase;
 
+  beforeAll(() => {
+    volunteersRepositoryInMemory = new VolunteerRepositoryInMemory();
+    createVolunteerUseCase = new CreateVolunteerUseCase(volunteersRepositoryInMemory);
+  });
+
+  it('should be able to create a new volunteer', async () => {
     const volunteerData: ICreateVolunteerRequestDTO = {
       fullName: 'Lore Ipsum',
-      email: 'jestTESTE@gmail.com',
+      email: 'newVolunteer@gmail.com',
       cellphoneNumberWithDDD: '(81) 99999-99999',
       occupation: OccupationOptions.MEDICO,
       expertise: 'Lore Ipsum',
@@ -24,6 +30,32 @@ describe('Create volunteer', () => {
       howDidKnowOfSDR: 'Lorem Ipsum',
     };
 
-    const volunteer = await createVolunteerUseCase.execute(volunteerData);
+    const checkUseCaseTest: boolean = await createVolunteerUseCase.execute(volunteerData);
+
+    expect(checkUseCaseTest).toBe(true);
+  });
+
+  it('should not be able to create an existing volunteer', async () => {
+    const volunteerData: ICreateVolunteerRequestDTO = {
+      fullName: 'Lore Ipsum',
+      email: 'existingVolunteer@gmail.com',
+      cellphoneNumberWithDDD: '(81) 99999-99999',
+      occupation: OccupationOptions.MEDICO,
+      expertise: 'Lore Ipsum',
+      listFreeDaysOfWeek: [
+        FreeDaysOfWeek.QUARTA,
+        FreeDaysOfWeek.QUINTA,
+      ],
+      experienceWithHealthy: 'Lorem Ipsum',
+      didParticipate: false,
+      numberOfParticipation: 0,
+      howDidKnowOfSDR: 'Lorem Ipsum',
+    };
+
+    await createVolunteerUseCase.execute(volunteerData);
+
+    await expect(createVolunteerUseCase.execute(volunteerData))
+      .rejects
+      .toEqual(new Error('Volunteer already exists / Voluntário já existe'));
   });
 });
