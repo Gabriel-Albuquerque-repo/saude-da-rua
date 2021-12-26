f4e0e45251f5970c424f4c0d75c84fab1a351cac
Gabriel de Albuquerque <gabrieleac2009@gmail.com>
2021-12-26 20:49:34 -0300

    [ADD] mongo in memory connection

--- tests/integration/CreateVolunteerController.spec.ts
+++ tests/integration/CreateVolunteerController.spec.ts
@@ -1,20 +1,27 @@
 import request from 'supertest';
 import express from 'express';
 import App from '../../src/app';
+import mongoInMemory from '../../testRepositories/mongodbIn-memory/MongoConnectionInMemory';
 import { OccupationOptions, FreeDaysOfWeek } from '../../src/entities/Volunteer';
 
 describe('Create volunteer Controller', () => {
   let app: express.Application;
 
-  beforeAll(() => {
+  beforeAll(async () => {
     app = new App().app;
+
+    await mongoInMemory.connect();
+  });
+
+  afterAll(async () => {
+    await mongoInMemory.close();
   });
 
   it('should able to create a new volunteer', async () => {
     const volunteerData = {
       fullName: 'Lore Ipsum',
       email: 'createVolunteerIntegrationTEST@gmail.com',
-      cellphoneNumberWithDDD: '(81) 99999-99999',
+      cellphoneNumberWithDDD: '(81) 99999-9999',
       occupation: OccupationOptions.MEDICO,
       expertise: 'Lore Ipsum',
       listFreeDaysOfWeek: [
@@ -32,6 +39,13 @@ describe('Create volunteer Controller', () => {
       .post('/create-volunteer')
       .send(volunteerData);
 
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(201);
+
+    const response2: request.Response = await request(app)
+      .post('/create-volunteer')
+      .send(volunteerData);
+
+    expect(response2.statusCode).toBe(200);
+    expect(response2.body.ErrorMessage).toEqual('Volunteer already exists / Voluntário já existe');
   });
 });
